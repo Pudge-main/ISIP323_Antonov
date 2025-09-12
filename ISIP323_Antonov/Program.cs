567c6c31b51ea3b42d7329b5420b0e42263f40ce
Pudge-main <sanytraly@gmail.com>
2025-09-12 15:12:18 +0300

    Сделал меню и пункты для него, ну и впринципе доделал работу.

--- ISIP323_Antonov/Program.cs
+++ ISIP323_Antonov/Program.cs
@@ -19,6 +19,91 @@ else
         sum += summa[i];
     }
 
+    void Menu()
+    {
+        Console.WriteLine("=======================================");
+        Console.WriteLine("Меню: ");
+        Console.WriteLine("Нажмите нужный номер, для выбора:");
+        Console.WriteLine("1.Вывод данных");
+        Console.WriteLine("2.Статистика (среднее, максимальное, минимальное, сумма)");
+        Console.WriteLine("3.Сортировка по цене (пузырьковая сортировка)");
+        Console.WriteLine("4.Конвертация валюты (пользователь вводит курс или выбирает из списка)");
+        Console.WriteLine("5.Поиск по названию");
+        Console.WriteLine("0.Выход");
+        Console.WriteLine("=======================================");
+        string ch = Console.ReadLine();
+        int choice = Convert.ToInt32(ch);
 
+        switch (choice)
+        {
+            case 1:
+                for (int i = 0; i < summa.Length; i++)
+                {
+                    Console.Write($"|{tovar[i]} -- {summa[i]}Руб.|\n");
+                }
+                Menu();
+                break;
+            case 2:
+                Console.WriteLine("Сумма всех операций: " + sum);
+                Console.WriteLine($"Максимальная стоимость: {summa.Max()}");
+                Console.WriteLine($"Минимальная стоимость: {summa.Min()}");
+                Console.WriteLine($"Средняя стоимость: {summa.Average()}");
+                Menu();
+                break;
+            case 3:
+                for (int i = 0; i < Count - 1; i++)
+                {
+                    for (int j = 0; j < Count - i - 1; j++)
+                    {
+                        if (summa[j] > summa[j + 1])
+                        {
+                            // меняем местами суммы
+                            double swapPrice = summa[j];
+                            summa[j] = summa[j + 1];
+                            summa[j + 1] = swapPrice;
+
+                            // меняем местами названия
+                            string swapInf = tovar[j];
+                            tovar[j] = tovar[j + 1];
+                            tovar[j + 1] = swapInf;
+                        }
+                    }
+                }
+                Console.WriteLine("\nОтсортированный список:");
+                for (int i = 0; i < Count; i++)
+                    Console.WriteLine($"{tovar[i]} — {summa[i]} руб.");
+                Menu();
+                break;
+            case 4:
+                Menu();
+                break;
+            case 5:
+                Console.WriteLine("Введите название: ");
+                string a = Console.ReadLine();
+                a.ToLower();
+                int num = 0;
+                for (int i = 0; i < tovar.Length; i++)
+                {
+                    if (tovar[i].Contains(a))
+                    {
+                        Console.WriteLine(tovar[i]);
+                        num++;
+                    }
+                }
+                if (num == 0)
+                {
+                    Console.WriteLine("Такого нету.");
+                }
+                Menu();
+                break;
+            case 0:
+                return;
+            default:
+                Console.WriteLine("Нету такого номера");
+                Menu();
+                break;
+        }
+    }
+    Menu();
 
 }
