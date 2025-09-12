ba57eb7b184a24231fb344b620fade0be2d58ec9
234919 <234919@KB37-120-C6>
2025-09-12 14:57:40 +0300

    Добавьте файлы проекта.

---
+++ ISIP323_Antonov/Program.cs
@@ -0,0 +1,24 @@
+﻿﻿Console.Write("Введите число операций: ");
+string C = Console.ReadLine();
+int Count = Convert.ToInt32(C);
+
+if (Count < 2 || Count > 40) Console.WriteLine("Некорректное кол-во операций!");
+else
+{
+    double sum = 0;
+    double[] cash = new double[Count];
+    string[] name = new string[Count];
+    Console.WriteLine("Введите информацию о операции по шаблону 'Название услуги или товара; Количество денег(в рублях)'");
+    for (int i = 0; i < Count; i++)
+    {
+        Console.Write($"Операция № {i + 1}: ");
+        string operation = Console.ReadLine();
+        string[] operations = operation.Split(';');
+        name[i] = operations[0];
+        cash[i] = Convert.ToDouble(operations[1]);
+        sum += cash[i];
+    }
+
+
+
+}
