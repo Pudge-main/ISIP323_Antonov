59a5f12bc12a56eeb1d815c8533d1246a22ca44e
234919 <234919@KB37-303-C8>
2025-09-24 11:52:26 +0300

    Добавлен базовый цикл программы ввод текста и проверка на минимальную длину

--- ISIP323_Antonov/Program.cs
+++ ISIP323_Antonov/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Введите текст (минимум 100 символов): ");
+            string userText = Console.ReadLine();
+
+            if (userText.Length < 100)
+            {
+                Console.WriteLine("Ошибка! Текст слишком короткий.");
+                continue;
+            }
+
+            Console.WriteLine("Текст принят!");
+            break;
+        }
+    }
+}
