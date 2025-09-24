6cc9c3ecc3c2fa25bfd608e85c50f72a118fd129
234919 <234919@KB37-303-C8>
2025-09-24 11:53:50 +0300

    Добавил поиск самого короткого или длинного слова и подсчёт гласных или согласных букв

--- ISIP323_Antonov/Program.cs
+++ ISIP323_Antonov/Program.cs
@@ -19,6 +19,14 @@ class Program
             string[] words = userText.Split(separators, StringSplitOptions.RemoveEmptyEntries);
             int wordCount = words.Length;
 
+            string shortestWord = words[0];
+            string longestWord = words[0];
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length < shortestWord.Length) shortestWord = words[i];
+                if (words[i].Length > longestWord.Length) longestWord = words[i];
+            }
+
             int sentenceCount = 0;
             for (int i = 0; i < userText.Length; i++)
             {
@@ -29,8 +37,26 @@ class Program
                 }
             }
 
+            string vowels = "аеёиоуыэюяАЕЁИОУЫЭЮЯaeiouyAEIOUY";
+            int vowelCount = 0;
+            int consonantCount = 0;
+
+            for (int i = 0; i < userText.Length; i++)
+            {
+                char ch = userText[i];
+                if (Char.IsLetter(ch))
+                {
+                    if (vowels.IndexOf(ch) != -1) vowelCount++;
+                    else consonantCount++;
+                }
+            }
+
             Console.WriteLine("Количество слов: " + wordCount);
+            Console.WriteLine("Самое короткое слово: " + shortestWord);
+            Console.WriteLine("Самое длинное слово: " + longestWord);
             Console.WriteLine("Количество предложений: " + sentenceCount);
+            Console.WriteLine("Гласные: " + vowelCount);
+            Console.WriteLine("Согласные: " + consonantCount);
             break;
         }
     }
