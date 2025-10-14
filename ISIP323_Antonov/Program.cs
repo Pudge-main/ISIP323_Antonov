f80902a6f8d1d076beb0d2257d182ab35ad8014c
234919 <234919@KB37-303-C9>
2025-10-14 13:40:37 +0300

    Декомпозиция, Добавлены классы Person, Student, Teacher, Course, University, Grade, Program

--- ISIP323_Antonov/Program.cs
+++ ISIP323_Antonov/Program.cs
@@ -1,24 +1,86 @@
-﻿﻿Console.Write("Введите число операций: ");
-string C = Console.ReadLine();
-int Count = Convert.ToInt32(C);
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-if (Count < 2 || Count > 40) Console.WriteLine("Некорректное кол-во операций!");
-else
+namespace UniversityApp
 {
-    double sum = 0;
-    double[] cash = new double[Count];
-    string[] name = new string[Count];
-    Console.WriteLine("Введите информацию о операции по шаблону 'Название услуги или товара; Количество денег(в рублях)'");
-    for (int i = 0; i < Count; i++)
+    public abstract class Person
     {
-        Console.Write($"Операция № {i + 1}: ");
-        string operation = Console.ReadLine();
-        string[] operations = operation.Split(';');
-        name[i] = operations[0];
-        cash[i] = Convert.ToDouble(operations[1]);
-        sum += cash[i];
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+
+        protected Person(string name, int age, string email) { }
+
+        public abstract void ShowInfo();
     }
 
+    public class Student : Person
+    {
+        public int StudentId { get; set; }
+        public List<Grade> Grades { get; set; }
 
+        public Student(string name, int age, string email, int studentId)
+            : base(name, age, email) { }
 
+        public void Enroll(Course course) { }
+        public List<Course> GetEnrolledCourses() { return null; }
+        public override void ShowInfo() { }
+    }
+    public class Teacher : Person
+    {
+        public int TeacherId { get; set; }
+
+        public Teacher(string name, int age, string email, int teacherId)
+            : base(name, age, email) { }
+
+        public void AssignCourse(Course course) { }
+        public override void ShowInfo() { }
+    }
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+        public Teacher AssignedTeacher { get; set; }
+        public List<Student> EnrolledStudents { get; set; }
+        public int MaxStudents { get; set; }
+
+        public Course(int courseId, string title, int maxStudents) { }
+
+        public bool EnrollStudent(Student student) { return false; }
+        public void RemoveStudent(Student student) { }
+        public void ShowInfo() { }
+    }
+
+    public class Grade
+    {
+        public int CourseId { get; set; }
+        public double Value { get; set; }
+    }
+
+    public class University
+    {
+        public List<Student> Students { get; set; }
+        public List<Teacher> Teachers { get; set; }
+        public List<Course> Courses { get; set; }
+
+        public University() { }
+
+        public void AddStudent(Student student) { }
+        public Student GetStudentById(int id) { return null; }
+
+        public void AddTeacher(Teacher teacher) { }
+        public Teacher GetTeacherById(int id) { return null; }
+
+        public void AddCourse(Course course) { }
+        public Course GetCourseById(int id) { return null; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа: система управления университетом (версия: декомпозиция)");
+        }
+    }
 }
